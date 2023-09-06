c6973bf473f491510bbc088ffdab6f06c51cffe8
Giothan <sergioalbino1080@gmail.com>
2023-09-06 19:39:15 -0300

    Mi Primer Commit

---
+++ src/main/java/com/greenappleexperience/demo/Tp1PersistenciaApplication.java
@@ -0,0 +1,13 @@
+package com.greenappleexperience.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1PersistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1PersistenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenappleexperience/demo/Tp1PersistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenappleexperience.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1PersistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
